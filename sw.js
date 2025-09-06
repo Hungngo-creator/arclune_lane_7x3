8cd63fac99cd1d65723bcc51d2e0af34eeb9d407
Hungngo-creator <ngo431228@gmail.com>
2025-09-06 16:21:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // bump version để ép cập nhật
-const CACHE = "Tap-to-Fly — Wave1 (Rift @49) v4";
+const CACHE = "Tap-to-Fly — Wave1 (Rift @49) v5";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", e => {
