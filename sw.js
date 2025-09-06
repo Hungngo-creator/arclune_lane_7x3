48959a2e48061418b6869341f830b88b49cd76da
Hungngo-creator <ngo431228@gmail.com>
2025-09-06 09:53:40 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // bump version để ép cập nhật
-const CACHE = "tapfly-v11";
+const CACHE = "tapfly-v12";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", e => {
