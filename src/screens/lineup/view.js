c6c980dfe2e654764592746d5b0c6ab044422035
Hungngo-creator <ngo431228@gmail.com>
2025-10-19 13:11:31 +0700

    Add files via upload

--- src/screens/lineup/view.js
+++ src/screens/lineup/view.js
@@ -1120,7 +1120,9 @@ function renderBenchDetails(){
       } else {
         cellEl.removeAttribute('title');
       }
-      const codeText = hasContent ? getUnitCode(unit, cell.label || '') : '';
+      const codeText = (!cell.unitId && hasContent)
+        ? getUnitCode(unit, cell.label || '')
+        : '';
       const avatarEl = document.createElement('div');
       avatarEl.className = 'lineup-bench__avatar';
       const avatarSource = unit?.avatar || cell.meta?.avatar || null;
