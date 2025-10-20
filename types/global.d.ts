7371c59e7dbe1016f90876f471da9f11c43e1537
Hungngo-creator <ngo431228@gmail.com>
2025-10-20 15:57:07 +0700

    Create global.d.ts

---
+++ types/global.d.ts
@@ -0,0 +1,13 @@
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.svg";
+declare module "*.webp";
+declare module "*.ogg";
+declare module "*.mp3";
+declare module "*.wav";
+declare module "*.glsl";
+declare module "*.frag";
+declare module "*.vert";
+declare module "*.json";
