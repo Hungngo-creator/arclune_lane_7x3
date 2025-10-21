fb36957a1b04807b5e6cbbd8db486986efb2fc58
Hungngo-creator <ngo431228@gmail.com>
2025-10-21 16:35:26 +0700

    Add files via upload

--- types/arclune-types.d.ts
+++ types/arclune-types.d.ts
@@ -1,3 +1,9 @@
 declare module '@types/vfx' {
-  export * from '../src/types/vfx.ts';
+  export type * from '../src/types/vfx';
+}
+
+declare module 'zod' {
+  export * from '../tools/zod-stub/index';
+  const z: typeof import('../tools/zod-stub/index').z;
+  export default z;;
 }
