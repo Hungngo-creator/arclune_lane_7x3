db5cf879a397ad23913c470805fb828677bb482d
Hungngo-creator <ngo431228@gmail.com>
2025-10-22 21:05:07 +0700

    Add files via upload

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,33 @@
 export type * from './units';
-export type * from './combat';
+export type {
+  StatusEffect,
+  StatusLifecyclePayload,
+  StatusDefinition,
+  StatusRegistry,
+  SkillCost,
+  SkillDefinition,
+  DamageContext,
+  LeaderSnapshot,
+  BattleDetail,
+  BattleResult,
+  BattleState,
+  SessionAIState,
+  PassiveSpec,
+  PassiveEffectArgs,
+  PassiveDefinition,
+  PassiveRegistry,
+} from './combat';
 export type * from './turn-order';
 export type * from './rng';
 export type * from './telemetry';
 export type * from './config';
 export type * from './utils';
 export type * from './vfx';
-export type * from './pve';
+export type {
+  RewardRoll,
+  WaveState,
+  EncounterState,
+  SessionRuntimeState,
+  CreateSessionOptions,
+  SessionState,
+} from './pve';
