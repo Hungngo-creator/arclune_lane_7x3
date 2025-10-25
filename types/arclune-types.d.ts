150981ac2cb7698b66278631a3ef809bfd385386
Hungngo-creator <ngo431228@gmail.com>
2025-10-25 17:58:02 +0700

    Add files via upload

--- types/arclune-types.d.ts
+++ types/arclune-types.d.ts
@@ -2,6 +2,14 @@ declare module '@types/vfx' {
   export type * from '../src/types/vfx';
 }
 
+declare module '@types/config' {
+  export { CatalogStatBlock, RosterUnitDefinition } from '../src/types/config.ts';
+}
+
+declare module '@types/units' {
+  export { UnitId } from '../src/types/units.ts';
+}
+
 declare module 'zod' {
   export * from '../tools/zod-stub/index';
   const z: typeof import('../tools/zod-stub/index').z;
