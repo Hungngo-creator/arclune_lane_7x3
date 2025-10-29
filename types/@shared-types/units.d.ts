243b5419e0b8f528a677f956c7c27c0407028e6a
Hungngo-creator <ngo431228@gmail.com>
2025-10-29 20:51:18 +0700

    Create units.d.ts

---
+++ types/@shared-types/units.d.ts
@@ -0,0 +1,15 @@
+export type {
+  UnitId,
+  Side,
+  StatBlock,
+  FuryState,
+  UnitToken,
+  SummonRequest,
+  QueuedSummonRequest,
+  SummonQueue,
+  QueuedSummonState,
+  ActionChainEntry,
+  ActionChainProcessedResult,
+} from '../../src/types/units.ts';
+
+export { createSummonQueue } from '../../src/types/units.ts';
