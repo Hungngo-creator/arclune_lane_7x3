84ee04a16ad6fc054dda5bc9fbcacf060c5c211b
Hungngo-creator <ngo431228@gmail.com>
2025-11-01 07:16:05 +0700

    Add files via upload

--- tools/typescript-transpiler/runtime.js
+++ tools/typescript-transpiler/runtime.js
@@ -1,10 +1,17 @@
 const ts = require('./vendor/typescript.js');
 
+const fallbackImportsNotUsedAsValues = {
+  Remove: 0,
+  Preserve: 1,
+  Error: 2,
+};
+
 module.exports = {
   transpileModule: ts.transpileModule,
   ModuleKind: ts.ModuleKind,
   ScriptTarget: ts.ScriptTarget,
   JsxEmit: ts.JsxEmit,
+  ImportsNotUsedAsValues: ts.ImportsNotUsedAsValues || fallbackImportsNotUsedAsValues,
   DiagnosticCategory: ts.DiagnosticCategory,
   flattenDiagnosticMessageText: ts.flattenDiagnosticMessageText,
   findConfigFile: ts.findConfigFile,
